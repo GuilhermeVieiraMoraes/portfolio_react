481e19f694719c8ad95c3dbcf07a211bc874a53d
GuilhermeVieiraMoraes <guims02@hotmail.com>
2026-01-28 16:21:50 -0300

    Enviando projeto para deploy

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Navbar</h1>
+      <h1>Portolio</h1>
+      <h1>Footer</h1>
+    </div>
+  )
+}
+
+export default App
